97489615af911638f445c61a3c26d3fb1724ed13
anoob-paul <155704736+anoob-paul@users.noreply.github.com>
2024-01-05 10:09:32 +0530

    Creating StringTest.py
    creating a string list and assigning the values to variables.

---
+++ StringTest.py
@@ -0,0 +1,14 @@
+#below code creates a list named fruits containing
+#three string elements 'apple', 'banana', and 'strawberry'.
+
+fruits = ['apple', 'banana', 'strawberry']
+
+x,y,z = fruits
+
+#It assigns the values from the fruits list to the variables x, y, and z in order. 
+# apple is assigned to x, banana is assigned to y, and 'strawberry' is assigned to z.
+#then print the variables
+
+print(x)
+print(y)
+print(z)
