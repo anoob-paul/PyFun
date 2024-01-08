efa9ccc0ef6519abb32c8469042dd05aa305a532
anoob-paul <155704736+anoob-paul@users.noreply.github.com>
2024-01-08 15:19:48 +0530

    Add files via upload

---
+++ bitwiseOps.py
@@ -0,0 +1,14 @@
+
+#bitwise operation works only on integers 
+n1, n2 = 20, 50
+
+#printing bitwise OR operations
+print('bitwise OR of',+n1,'and',+n2,'is :', n1 | n2)  
+
+#printing bitwise AND operations
+print('bitwise AND of',+n1,'and',+n2,'is :', n1 & n2)  
+
+#printing bitwise XOR operations
+print('bitwise XOR of',+n1,'and',+n2,'is :', n1 ^ n2)  
+
+print('bitwise NOT ~ of',+n2,'is :', ~n2)
