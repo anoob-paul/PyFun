c212f2a6abf622729444d4652bb1bcab6aa0201e
Anoob Paul <155704736+anoobpaul@users.noreply.github.com>
2024-02-16 22:58:12 +0530

    Create BestTimetoBuyandSellStocks.py
    In priogress version

---
+++ BestTimetoBuyandSellStocks.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit =0
+        buy =prices[0]
+
+        while (price in prices):
+            buy =min(pri)
+                profit = max(profit, prices[r]-prices[l])
+            else :
+                r=l
+
+        return profit      
